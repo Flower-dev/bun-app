06dd4f0d81897a1c94de11d1b89b42400be8beaf
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2025-01-12 18:46:37 +0100

    front : install shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,21 @@ import "@blocknote/core/fonts/inter.css";
 import { BlockNoteView } from "@blocknote/mantine";
 import "@blocknote/mantine/style.css";
 import { useCreateBlockNote } from "@blocknote/react";
+import { Button } from "@/components/ui/button"
+import "./index.css"
  
 export default function App() {
   // Creates a new editor instance.
   const editor = useCreateBlockNote();
  
   // Renders the editor instance using a React component.
-  return <BlockNoteView editor={editor} />;
+  return(
+<>
+<Button>Click me</Button>
+
+<BlockNoteView editor={editor} />
+</>
+
+  ) ;
 }
  
